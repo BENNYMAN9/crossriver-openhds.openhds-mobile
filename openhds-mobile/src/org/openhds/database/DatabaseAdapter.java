7b37dfc0a96b79fcf9883d02e68edadab2f9d999
Brian Hartsock <brian.hartsock@maine.edu>
2012-06-21 16:51:00 -0400

    Locations can now be downloaded to the phone.

--- openhds-mobile/src/org/openhds/database/DatabaseAdapter.java
+++ openhds-mobile/src/org/openhds/database/DatabaseAdapter.java
@@ -5,9 +5,12 @@ import android.content.Context;
 import android.database.SQLException;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
 
 public class DatabaseAdapter {
 	
+	private static final String TAG = "DatabaseAdapter";
+		
 	private static final String DATABASE_NAME = "entityData";
 	
 	private static final String DATABASE_TABLE_INDIVIDUAL = "individual";
@@ -21,6 +24,13 @@ public class DatabaseAdapter {
 	private static final String INDIVIDUAL_FATHER = "father";  
 	
 	private static final String DATABASE_TABLE_LOCATION = "location";
+	private static final String LOCATION_UUID = "uuid";  
+	private static final String LOCATION_EXTID = "extId";  
+	private static final String LOCATION_NAME = "name";  
+	private static final String LOCATION_LATITUDE = "latitude";  
+	private static final String LOCATION_LONGITUDE = "longitude";  
+	private static final String LOCATION_HIERARCHY = "hierarchy";	
+
 	private static final String DATABASE_TABLE_SOCIALGROUP = "socialgroup";
 	private static final String DATABASE_TABLE_VISIT = "visit";
 	private static final String DATABASE_TABLE_RESIDENCY = "residency";
@@ -35,6 +45,11 @@ public class DatabaseAdapter {
 	        "dob text not null, gender text not null, mother text not null, " +
 	        "father text not null, foreign key(mother) references individual(uuid), " +
 	        "foreign key(father) references individual(uuid));";
+	
+	private static final String LOCATION_CREATE =
+        "create table location (uuid text primary key, " + 
+        "extId text not null, name text not null, latitude text, " +
+        "longitude text, hierarchy text not null);";
 	 
 	 private DatabaseHelper dbHelper;
 	 private SQLiteDatabase database;
@@ -65,8 +80,23 @@ public class DatabaseAdapter {
 		 values.put(INDIVIDUAL_GENDER, gender);
 		 values.put(INDIVIDUAL_MOTHER, mother);
 		 values.put(INDIVIDUAL_FATHER, father);
+		 Log.i(TAG, "inserting into individual with extId " + extId);
 		 return database.insert(DATABASE_TABLE_INDIVIDUAL, null, values);	
 	}
+	 
+	 public long createLocation(String uuid, String extId, String name, String latitude, 
+			 String longitude, String hierarchy) {
+		 
+		 ContentValues values = new ContentValues();
+		 values.put(LOCATION_UUID, uuid);
+		 values.put(LOCATION_EXTID, extId);
+		 values.put(LOCATION_NAME, name);
+		 values.put(LOCATION_LONGITUDE, longitude);
+		 values.put(LOCATION_LATITUDE, latitude);
+		 values.put(LOCATION_HIERARCHY, hierarchy);
+		 Log.i(TAG, "inserting into location with extId " + extId);
+		 return database.insert(DATABASE_TABLE_LOCATION, null, values);
+	 }
 	 	 
 	 public SQLiteDatabase getDatabase() {
 		 return database;
@@ -85,11 +115,13 @@ public class DatabaseAdapter {
 		 @Override
 		 public void onCreate(SQLiteDatabase db) {
 			 db.execSQL(INDIVIDUAL_CREATE);
+			 db.execSQL(LOCATION_CREATE);
 		 }
 	
 		 @Override
 		 public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
 			 db.execSQL("drop table if exists " + DATABASE_TABLE_INDIVIDUAL);
+			 db.execSQL("drop table if exists " + DATABASE_TABLE_LOCATION);
 		     onCreate(db);
 		 }
 	 }
--- openhds-mobile/src/org/openhds/task/SyncEntitiesTask.java
+++ openhds-mobile/src/org/openhds/task/SyncEntitiesTask.java
@@ -26,7 +26,6 @@ import org.xmlpull.v1.XmlPullParserFactory;
 import android.app.ProgressDialog;
 import android.content.Context;
 import android.os.AsyncTask;
-import android.os.Looper;
 
 /**
  * An AsyncTask to upload all OpenHDS data from the web service configured in ServerPreferences.
@@ -70,9 +69,14 @@ public class SyncEntitiesTask extends AsyncTask<Void, String, Boolean> {
 		 
 		 try {
 			entity = "individual";
-			processUrl(baseurl + "/individual");
+			processUrl(baseurl + "/individual");	
+			resetDialogParams();
+			
+			entity = "location";
+			processUrl(baseurl + "/location");
 		 } 
 		 catch (Exception e) {
+			e.printStackTrace();
 			return false;
 		 }
 		 return true;
@@ -114,6 +118,7 @@ public class SyncEntitiesTask extends AsyncTask<Void, String, Boolean> {
 	private void setupDB() {	
 		databaseAdapter.open();
 		databaseAdapter.getDatabase().delete("individual", null, null);
+		databaseAdapter.getDatabase().delete("location", null, null);
 		databaseAdapter.close();
 	}
 	
@@ -143,13 +148,14 @@ public class SyncEntitiesTask extends AsyncTask<Void, String, Boolean> {
                     	dialog.setMax(count);
                     	parser.nextTag();
                     }
-                    
-                    if (name.equalsIgnoreCase("individual")) {
+                    else if (name.equalsIgnoreCase("individual") && entity.equals("individual")) {
                     	processIndividualParams(parser);
                     	activity.runOnUiThread(changeMessageIndividual);
                     }
-                     
-       
+                    else if (name.equalsIgnoreCase("location") && entity.equals("location")) {
+                    	processLocationParams(parser);
+                    	activity.runOnUiThread(changeMessageLocation);
+                    }
                     break;
             }
             eventType = parser.next();
@@ -161,7 +167,41 @@ public class SyncEntitiesTask extends AsyncTask<Void, String, Boolean> {
 	        dialog.setMessage("Downloading Individuals");
 	    }
 	};
+	
+	private Runnable changeMessageLocation = new Runnable() {
+	    public void run() {
+	        dialog.setMessage("Downloading Locations");
+	    }
+	};
 
+	private void processLocationParams(XmlPullParser parser) throws XmlPullParserException, IOException {
+		String name = "";
+		Map<String, String> paramMap = new HashMap<String, String>();
+        parser.nextTag();
+        name = parser.getName();
+        paramMap.put("extId", parser.nextText());
+        parser.nextTag();
+        name = parser.getName();
+        paramMap.put("hierarchy", parser.nextText());
+        parser.nextTag();
+        name = parser.getName();
+        paramMap.put("latitude", parser.nextText());
+        parser.nextTag();
+        name = parser.getName();
+        paramMap.put("longitude", parser.nextText());
+        parser.nextTag();
+        name = parser.getName();
+        paramMap.put("name", parser.nextText());
+        parser.nextTag();
+        name = parser.getName();
+        paramMap.put("uuid", parser.nextText());
+        parser.nextTag();
+        
+        saveLocationToDB(paramMap.get("uuid"), paramMap.get("extId"), paramMap.get("name"), 
+        		paramMap.get("latitude"), paramMap.get("longitude"), paramMap.get("hierarchy"));
+        
+        dialog.incrementProgressBy(1);
+	}
 	
 	private void processIndividualParams(XmlPullParser parser) throws XmlPullParserException, IOException {
 		String name = "";
@@ -199,6 +239,11 @@ public class SyncEntitiesTask extends AsyncTask<Void, String, Boolean> {
         dialog.incrementProgressBy(1);
 	}
 	
+	private void resetDialogParams() {
+		dialog.setProgress(0);
+		dialog.setMax(0);
+	}
+	
 	protected void onPostExecute(final Boolean result) {
 		dialog.setProgress(0);
 	    listener.collectionComplete(result);
@@ -207,6 +252,12 @@ public class SyncEntitiesTask extends AsyncTask<Void, String, Boolean> {
 	public void saveIndividualToDB(String uuid, String extId, String firstName, String lastName, String gender, String dob, String mother, String father) {
 	    databaseAdapter.open();
 	    databaseAdapter.createIndividual(uuid, extId, firstName, lastName, dob, gender, mother, father);
+	    databaseAdapter.close();
+	}
+	
+	public void saveLocationToDB(String uuid, String extId, String name, String latitude, String longitude, String hierarchy) {
+	    databaseAdapter.open();
+	    databaseAdapter.createLocation(uuid, extId, name, latitude, longitude, hierarchy);
 	    databaseAdapter.close();
 	}
 }
